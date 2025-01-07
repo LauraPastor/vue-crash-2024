83135f27638fff615e9008271469b3f206651738
LauraPastor <laurapastorj@gmail.com>
2025-01-07 20:22:33 +0100

    Add edit and add job functionality

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -10,8 +10,7 @@
             <h1 class="text-3xl font-bold mb-4">{{ state.job.title }}</h1>
             <div
               class="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
-              <i
-                class="fa-solid fa-location-dot text-lg text-orange-700 mr-2"></i>
+              <i class="pi pi-map-marker text-lg text-orange-700 mr-2"></i>
               <p class="text-orange-700">{{ state.job.location }}</p>
             </div>
           </div>
@@ -67,6 +66,7 @@
               >Edit Job</RouterLink
             >
             <button
+              @click="deleteJob"
               class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
               Delete Job
             </button>
@@ -81,16 +81,37 @@
 import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
 import axios from 'axios';
 import { reactive, onMounted } from 'vue';
-import { useRoute, RouterLink } from 'vue-router';
+import { useRoute, RouterLink, useRouter } from 'vue-router';
+import { useToast } from 'vue-toastification';
 import BackButton from '@/components/BackButton.vue';
 
 const route = useRoute();
+const router = useRouter();
+const toast = useToast();
+
 const jobId = route.params.id;
 
 const state = reactive({
   job: {},
   isLoading: true,
 });
+
+const deleteJob = async () => {
+  try {
+    const confirmDelete = window.confirm(
+      'Are you sure you want to delete this job?'
+    );
+    if (confirmDelete) {
+      await axios.delete(`/api/jobs/${jobId}`);
+      toast.success('Job deleted successfully');
+      router.push('/jobs');
+    }
+  } catch (error) {
+    console.error('Error deleting job', error);
+    toast.error('Error deleting job');
+  }
+};
+
 onMounted(async () => {
   try {
     const response = await axios.get(`/api/jobs/${jobId}`);
